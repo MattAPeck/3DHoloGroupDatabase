28b3773d8a0d0bb76b4bd033a3006683b0a7782a
MattAPeck <MatthewAlexanderPeck@gmail.com>
2023-08-07 23:05:52 -0400

    Initial Commit

--- Database.py
+++ Database.py
@@ -1,5 +1,4 @@
 import pyodbc
-import datetime
 
 # I know I did alot of this explanation in my first readme file explaining how this works,
 # but I figured I should go through a more thorough walkthrough in how this works.
@@ -19,12 +18,13 @@ conn = pyodbc.connect(
 # These are our SQL statements, you can write SQL in Python just like a normal Query and then you can just
 # send it to the database to run the SQL on their end, with the values we send it.
 INSERT_ESTABLISHMENT = "INSERT INTO Establishment(Latitude, Longitude, DateLastModified, EstablishmentName, " \
-                       "EstablishmentType, DateCreated) " \
-                       "VALUES (?, ?, ?, ?, ?, ?);"
+                       "EstablishmentType, DateCreated, MostUsedSentenceHistoryId) " \
+                       "VALUES (?, ?, ?, ?, ?, ?, ?);"
 # When you pick your values for the database, Pyodbc has us use ? for user input.
 # The order that you set this up in is important, so keep track when you assign variables down the line.
 UPDATE_ESTABLISHMENT = "UPDATE Establishment SET Latitude = ?, Longitude = ?, DateLastModified = ?, " \
                        "EstablishmentName = ?, EstablishmentType = ? WHERE EstablishmentId = ?;"
+# Potentially change above statement to be WHERE MostUsedSentenceHistoryId = ? instead of EstablishmentId.
 
 FIND_ESTABLISHMENT = "SELECT * FROM Establishment WHERE EstablishmentType = ?;"
 
@@ -32,24 +32,27 @@ FIND_ESTABLISHMENT_TYPES = "SELECT EstablishmentId, EstablishmentType FROM Estab
 
 INSERT_SENTENCE = "INSERT INTO SentenceByEstablishment(Sentence, EstablishmentId) VALUES (?, ?);"
 
-# Potential SQL for retrieving the Lng/Lat coords from the database for implementation.
-RECEIVE_COORDINATES = "SELECT Longitude, Latitude FROM MostUsedSentenceHistory WHERE EstablishmentId = ?;"
+
+RECEIVE_MOSTUSEDSENTENCEHISTORYID = "SELECT MostUsedSentenceHistoryId, Longitude, Latitude " \
+                                    "FROM MostUsedSentenceHistory;"
+
+RECEIVE_COORDINATES = "SELECT Longitude, Latitude FROM MostUsedSentenceHistory;"
 
 
 # Notice here that I have the variables in the same order as listed in the SQL itself.
 # That is important, because that is the data it expects to receive.
 # If your variables are in the wrong order/are the wrong type then it won't get an error on this side,
 # but it will be an error on the Server side, so the data won't be added to the tables.
-def create_establishment(latitude, longitude, datemodified, name, type, datecreated):
+def create_establishment(latitude, longitude, datemodified, name, type, datecreated, mostusedsentencehistoryid):
     cursor = conn.cursor()  # Every time you interact with the database you will create a cursor.
     # That Cursor will be what you use to execute you SQL.
-    cursor.execute(INSERT_ESTABLISHMENT, (latitude, longitude, datemodified, name, type, datecreated))
+    cursor.execute(INSERT_ESTABLISHMENT, (latitude, longitude, datemodified, name, type, datecreated, mostusedsentencehistoryid))
     # Whenever you add something to the database you need to make sure you commit it,
     # otherwise it won't actually save server side.
     cursor.commit()
 
 
-def update_establishment(latitude, longitude, datemodified, name, type, id):
+def update_establishment(latitude, longitude, datemodified, name, type, id):  # TODO Probably to be changed.
     cursor = conn.cursor()
     cursor.execute(UPDATE_ESTABLISHMENT, (latitude, longitude, datemodified, name, type, id))
     cursor.commit()
@@ -75,9 +78,12 @@ def create_sentence(text, Id):
     cursor.commit()
 
 
-# TODO We need to reconfigure the database and add the code in main to retrieve
-#  the coordinates from MostUsedSentenceHistory that we will use.
+def get_mostusedid():
+    cursor = conn.cursor()
+    cursor.execute(RECEIVE_MOSTUSEDSENTENCEHISTORYID)
+    return cursor.fetchall()
+
 def get_coordinates():
     cursor = conn.cursor()
     cursor.execute(RECEIVE_COORDINATES)
-    return cursor.fetchone()
+    return cursor.fetchall()
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import Database
 import datetime
 import googlemaps
-import geopy
-from geopy.geocoders import Nominatim
 from transformers import pipeline, set_seed
 
 # This is our testing menu. I just use this format to test all of our programs with user_input
@@ -10,22 +8,15 @@ from transformers import pipeline, set_seed
 menu = '''\n Please Select what you would like to do.
 1. Insert new data.
 2. Update existing data.
-3. Find Existing data.
-4. Generate Sentences.
-5. Insert Sentence to table.
-6. Prints DateTime timestamp for testing purposes.
-7. Exit.
+3. Generate Sentences and add to the table.
+4. Prints DateTime timestamp for testing purposes.
+5. Exit.
 
 Your Selection: '''
 # TODO
 #  ---------------------------------------------------------------------------------------------------------------------
-#  Rethink Database relationship between MostUsedCommonSentenceHistory and Establishment.
-#  Move MostUsedSentenceHistoryId in Establishment Table and removing EstablishmentId
-#  Loop over MostUsedSentenceHistoryTable WHERE MostUsedSentenceHistoryId IS NOT IN Establishment(table)
-#  Reconsider/look into Geopy for potential reliability issues.
-#  Alternatively, find out how to get Establishment name out of GoogleMapsAPI.
-#  Create/implement the database call we need to retrieve the Lng/Lat coordinates from MostUsedSentenceHistory.
 #  Delete unneeded comments.
+#  Delete code blocks that won't go into final product.
 #  ---------------------------------------------------------------------------------------------------------------------
 # Keep in mind that not all of what is currently in this program will be implemented.
 # Some of this is more to demonstrate and test our connection to the database and make sure our SQL is working properly.
@@ -35,47 +26,45 @@ def prompt_add_data():
     # This is our GoogleMaps API, and the Key that we will use.
     # The googlemaps api is how we will be getting our establishment types for a specific address.
     gmaps = googlemaps.Client(key='AIzaSyD166DaA9hYm7HaES8a8OePA4R9htLSmWQ')
-    latitude = 40.1384284  # Hardcoded for now, but pulling/looping from MostUsedSentencesHistory in future
-    longitude = -88.2586279
-    rever_geocode_result = gmaps.reverse_geocode((latitude, longitude))
+    MostUsedSentenceHistoryId = Database.get_mostusedid()
+    for data in MostUsedSentenceHistoryId:
+        print(data)  # For testing purposes. Remove upon full implementation.
+        mostusedsentencehistoryid = data[0]  # The database call SQL puts the variables in this order.
+        longitude = data[1]  # Any change there will break this code.
+        latitude = data[2]
+        rever_geocode_result = gmaps.reverse_geocode((latitude, longitude))
 
-    # The googlemaps API doesn't give us the actual name of whatever is located at an address,
-    # so we need another API for that. We tried Geopy, but we are looking for alternatives.
-    """locator = Nominatim(user_agent='myGeocoder')
-    coordinates = '40.1419295, -88.2547879'
-    location = locator.reverse(coordinates)
-    name = location.address[0]"""
-    name = 'Red Lobster' # Temporary hardcode, will use whatever new api/method for the name.
+        if len(rever_geocode_result) == 0:
+            continue
+        else:
+            place_id = rever_geocode_result[0]['place_id']
+        place_detail = gmaps.place(place_id)
 
-    # Since this is us inserting information about an establishment for the first time,
-    # the datemodified and datecreated are both the same.
-    datemodified = datetime.datetime.now()
-    datecreated = datetime.datetime.now()
+        datemodified = datetime.datetime.now()  # First data insert. DateModified/Created are done at same time.
+        datecreated = datetime.datetime.now()
 
     # This loop is needed because our gmaps result actually gives us multiple 'types' that an establishment
     # is classified as, and this is how we make sure all of them are recorded in separate rows for our use.
-    # It's more database calls, but that isn't a long process to begin with,
-    # and one that is fixed with a possible implementation of a connection pool in the future.
-    for types in rever_geocode_result[0]['types']:
-        type = types
-        Database.create_establishment(latitude, longitude, datemodified, name, type, datecreated)
+        for types in rever_geocode_result[0]['types']:
+            type = types
+            name = place_detail['result']['name']
+            Database.create_establishment(latitude, longitude, datemodified, name, type, datecreated, mostusedsentencehistoryid)
 
 
 def prompt_update_data():
     # TODO This entire function will need to be automated, much in the way that the previous function is/was.
-    latitude = input('Latitude of location: ')
-    longitude = input('Longitude of location: ')
-    datemodified = datetime.datetime.now()
-    name = input('Enter Establishment Name: ')
-    type = input('Enter Establishment Type: ')
-    id = input('Enter EstablishmentId: ')
+    #  For future automation we should probably follow the format from above. That's if we even need this at all.
+    # coordinates = Database.get_coordinates()
+    # for data in coordinates:
+    latitude = input('Latitude of location: ')  # data[1]
+    longitude = input('Longitude of location: ')  # data[0]
+    datemodified = datetime.datetime.now()  # Stays the same.
+    name = input('Enter Establishment Name: ')  # I don't think we need to use GoogleMaps for this but we might.
+    type = input('Enter Establishment Type: ')  # Same here, we might need to use the same Googlemaps format.
+    id = input('Enter EstablishmentId: ')  # If we implment this we DEFINITELY need to change this,
+    # due the the new format in the database... unless we just use MostUsedSentenceHistoryId instead of establishmentId.
+    # In that case we need to change the SQL in Database too.
     Database.update_establishment(latitude, longitude, datemodified, name, type, id)
-
-
-# TODO Delete.
-def prompt_find_data():
-    name = input('Enter the name of your desired establishment: ')
-    return name
 
 
 def sentence_generate():
@@ -93,28 +82,12 @@ def sentence_generate():
         # return id
 
 
-# TODO Remove this, we don't need this anymore with how the database design plans have changed.
-def prompt_add_sentence():
-    establishmentType = Database.get_establishment_type()
-    for type in establishmentType:
-        type = type[1]
-        if type == 'Retail':
-            type = 1
-            return type
-        elif type == 'Grocery':
-            type = 2
-            return type
-        elif type == 'Hardware':
-            type = 3
-            return type
-
-
 # This is the function that tells Python where to start working. It will compile all code above this,
 # but it should only run what is below this.
 if __name__ == "__main__":
     # For now I use a while loop to keep everything running while we test, but this will be removed once
     # full implementation is done.
-    while (user_input := input(menu)) != '7':
+    while (user_input := input(menu)) != '5':
 
         if user_input == '1':
             prompt_add_data()
@@ -123,22 +96,9 @@ if __name__ == "__main__":
             prompt_update_data()
 
         elif user_input == '3':
-            name = prompt_find_data()
-            establishment = Database.find_data(name)
-            if establishment is not None:
-                print(establishment)
-            else:
-                print('There is no data about an establishment with that name, please try again.')
-
-        elif user_input == '4':
             sentence_generate()
 
-        elif user_input == '5':
-            sentence = sentence_generate()
-            type = prompt_add_sentence()
-            Database.create_sentence(sentence, type)
-
-        elif user_input == '6':
+        elif user_input == '4':
             print(datetime.datetime.now())
 
         else:
