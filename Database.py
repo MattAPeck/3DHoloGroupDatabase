5554f05a1f38c9b84f2ea0b9f73c590637e6dbd7
MattAPeck <108022416+MattAPeck@users.noreply.github.com>
2023-10-31 15:01:49 -0400

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,13 +1,6 @@
 import pyodbc
 
-# I know I did alot of this explanation in my first readme file explaining how this works,
-# but I figured I should go through a more thorough walkthrough in how this works.
-
-# This is where our Server information goes, for testing purposes this will be unique per each of us.
-# In the final implementation we will probably use somthing called a .env file to store this information.
-# That way the program can access it, but any random person who happens upon our repository won't be able to just have
-# the access information for the 3DHoloGroup server.
-server = 'MATTHEWSPC'
+server = 'server'
 database = 'Dummy Database'
 username = 'python'
 password = '123'
@@ -15,16 +8,12 @@ conn = pyodbc.connect(
     'DRIVER={ODBC Driver 18 for SQL Server};SERVER=' + server + ';DATABASE=' + database +
     ';ENCRYPT=no; UID=' + username + ';PWD=' + password)
 
-# These are our SQL statements, you can write SQL in Python just like a normal Query and then you can just
-# send it to the database to run the SQL on their end, with the values we send it.
 INSERT_ESTABLISHMENT = "INSERT INTO Establishment(Latitude, Longitude, DateLastModified, EstablishmentName, " \
                        "EstablishmentType, DateCreated, MostUsedSentenceHistoryId) " \
                        "VALUES (?, ?, ?, ?, ?, ?, ?);"
-# When you pick your values for the database, Pyodbc has us use ? for user input.
-# The order that you set this up in is important, so keep track when you assign variables down the line.
+
 UPDATE_ESTABLISHMENT = "UPDATE Establishment SET Latitude = ?, Longitude = ?, DateLastModified = ?, " \
                        "EstablishmentName = ?, EstablishmentType = ? WHERE EstablishmentId = ?;"
-# Potentially change above statement to be WHERE MostUsedSentenceHistoryId = ? instead of EstablishmentId.
 
 FIND_ESTABLISHMENT = "SELECT * FROM Establishment WHERE EstablishmentType = ?;"
 
@@ -39,16 +28,9 @@ RECEIVE_MOSTUSEDSENTENCEHISTORYID = "SELECT MostUsedSentenceHistoryId, Longitude
 RECEIVE_COORDINATES = "SELECT Longitude, Latitude FROM MostUsedSentenceHistory;"
 
 
-# Notice here that I have the variables in the same order as listed in the SQL itself.
-# That is important, because that is the data it expects to receive.
-# If your variables are in the wrong order/are the wrong type then it won't get an error on this side,
-# but it will be an error on the Server side, so the data won't be added to the tables.
 def create_establishment(latitude, longitude, datemodified, name, type, datecreated, mostusedsentencehistoryid):
-    cursor = conn.cursor()  # Every time you interact with the database you will create a cursor.
-    # That Cursor will be what you use to execute you SQL.
+    cursor = conn.cursor()
     cursor.execute(INSERT_ESTABLISHMENT, (latitude, longitude, datemodified, name, type, datecreated, mostusedsentencehistoryid))
-    # Whenever you add something to the database you need to make sure you commit it,
-    # otherwise it won't actually save server side.
     cursor.commit()
 
 
@@ -61,8 +43,6 @@ def update_establishment(latitude, longitude, datemodified, name, type, id):  # 
 def find_data(type):
     cursor = conn.cursor()
     cursor.execute(FIND_ESTABLISHMENT, (type))
-    # When you want to retrieve something from the database, you use either fetchall, for all values in the column,
-    # or fetchone for a single value.
     return cursor.fetchall()
 
 
